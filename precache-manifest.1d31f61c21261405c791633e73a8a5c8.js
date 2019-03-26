8fd7890730e875f86504caf20aebfd98a1640843
laralcq <laraloicq@gmail.com>
2019-03-26 12:38:43 +0100

    Updates

---
+++ precache-manifest.1d31f61c21261405c791633e73a8a5c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65631388134343e96807",
+    "url": "/LaraLcq/react-character-manager/static/css/main.d809ea13.chunk.css"
+  },
+  {
+    "revision": "65631388134343e96807",
+    "url": "/LaraLcq/react-character-manager/static/js/main.4daf0133.chunk.js"
+  },
+  {
+    "revision": "03294b54a8194ab6a113",
+    "url": "/LaraLcq/react-character-manager/static/js/runtime~main.cba0876b.js"
+  },
+  {
+    "revision": "85dae6156dcb48047b1d",
+    "url": "/LaraLcq/react-character-manager/static/css/2.520163c4.chunk.css"
+  },
+  {
+    "revision": "85dae6156dcb48047b1d",
+    "url": "/LaraLcq/react-character-manager/static/js/2.c44f2732.chunk.js"
+  },
+  {
+    "revision": "2b61d22dacbfd0ab3831e7362efecebe",
+    "url": "/LaraLcq/react-character-manager/index.html"
+  }
+];
